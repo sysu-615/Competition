f117b9b97f36ed62f24cbb4944444c1d32273661
liuyh73 <7home7Tmade>
2018-05-25 18:56:51 +0800

    update

---
+++ Competition/Models/NavMenuMatchItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competition.Models
+{
+    class NavMenuMatchItem
+    {
+        // 赛事
+        public NavMenuItem Match { get; set; }
+        // 运动员信息
+        public NavMenuItem AthleteInfo { get; set; }
+        // 对战信息
+        public NavMenuItem BattleInfo { get; set; }
+        // 结果信息
+        public NavMenuItem ResultInfo { get; set; }
+
+        public NavMenuMatchItem(NavMenuItem _Match, NavMenuItem _AthleteInfo, NavMenuItem _BattleInfo, NavMenuItem _ResultInfo)
+        {
+            this.Match = _Match;
+            this.AthleteInfo = _AthleteInfo;
+            this.BattleInfo = _BattleInfo;
+            this.ResultInfo = _ResultInfo;
+        }
+    }
+}
---
+++ Competition/ViewModels/NavMenuMatchItemVM.cs
@@ -0,0 +1,64 @@
+﻿using Competition.Models;
+using Competition.Views.MatchInfo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Competition.ViewModels
+{
+    class NavMenuMatchItemVM
+    {
+        private static NavMenuMatchItemVM navMenuMatchItemVM = null;
+
+        public static NavMenuMatchItemVM GetNavMenuMatchItemVM()
+        {
+            if (navMenuMatchItemVM == null)
+                navMenuMatchItemVM = new NavMenuMatchItemVM();
+            return navMenuMatchItemVM;
+        }
+        private NavMenuMatchItemVM() { }
+        private ObservableCollection<NavMenuMatchItem> allNavMenuMatchItem = new ObservableCollection<NavMenuMatchItem>(new[]
+            {
+                new NavMenuMatchItem(
+                    new NavMenuItem()
+                    {
+                        prop="网球",
+                        symbol = Symbol.Globe,
+                        text = "网球",
+                        Selected = Visibility.Visible,
+                        destPage = typeof(Athletes)
+                    },
+                    new NavMenuItem()
+                    {
+                        prop="网球",
+                        symbol = Symbol.People,
+                        text = "运动员",
+                        Selected = Visibility.Visible,
+                        destPage = typeof(Athletes)
+                    },
+                    new NavMenuItem()
+                    {
+                        prop="网球",
+                        symbol = Symbol.LikeDislike,
+                        text = "对战",
+                        Selected = Visibility.Visible,
+                        destPage = typeof(Battles)
+                    },
+                    new NavMenuItem()
+                    {
+                        prop="网球",
+                        symbol = Symbol.ViewAll,
+                        text = "胜负",
+                        Selected = Visibility.Visible,
+                        destPage = typeof(Results)
+                    })
+                
+            });
+        public ObservableCollection<NavMenuMatchItem> AllNavMenuMatchItem { get { return this.allNavMenuMatchItem; } }
+    }
+}
